54a253e66b889f0bd66d17b9e9b9120f48951582
oleg_nesterchuk <oleg.n@reply.io>
2021-08-03 11:52:16 +0300

    Initial commit

---
+++ Querier/EmailReportModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Querier
+{
+    public class EmailReportModel
+    {
+        public int Id { get; set; }
+        public bool IsBounced { get; set; }
+        public int ProspectId { get; set; }
+        public int CampaignProspectStatus { get; set; }
+        public int OpensCount { get; set; }
+        public bool IsReplied { get; set; }
+        public bool IsOptedOut { get; set; }
+        public int UserId { get; set; }
+        public int CampaignId { get; set; }
+        public string CampaignName { get; set; }
+        public int CampaignOwnerId { get; set; }
+        public int ClicksCount { get; set; }
+        public int UniqueClicksCount { get; set; }
+        public string CampaignReplyInboxName { get; set; }
+        public DateTime? BounceDate { get; set; }
+        public string UserName { get; set; }
+        public DateTime Date { get; set; }
+        public string EmailAccountEmail { get; set; }
+        public int? EmailTemplateId { get; set; }
+        public int? CampaignStepEmailId { get; set; }
+        public int SequenceStepId { get; set; }
+        public int CampaignProspectSubStatus { get; set; }
+        public DateTime? ReplyDate { get; set; }
+        public DateTime? ManualReplyDate { get; set; }
+        public string EmailTemplateName { get; set; }
+        public bool InvalidEmail { get; set; }
+    }
+}
---
+++ Querier/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Querier
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            const string connectionString = "";
+            const string query = "";
+
+            var sw = Stopwatch.StartNew();
+            
+            var connection = new SqlConnection(connectionString);
+            var results = await connection.QueryAsync<EmailReportModel>(query);
+
+            var list = results.ToList();
+            
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed.TotalSeconds);
+        }
+    }
+}
